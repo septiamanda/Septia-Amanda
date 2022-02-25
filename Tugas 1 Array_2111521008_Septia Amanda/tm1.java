92fc848c5716f6b1da98e048a67ae1e7389f86b4
septiamanda <100399958+septiamanda@users.noreply.github.com>
2022-02-26 00:17:18 +0700

    Add files via upload

---
+++ Tugas 1 Array_2111521008_Septia Amanda/tm1.java
@@ -0,0 +1,16 @@
+public class tm1
+{
+    public static void main(String[] args) 
+    {
+        String[] nama= {"A","N","D","A"};
+
+        //Soal no 1
+        //Tentukan hasil yang didapatkan ketika dilakukan perintah berikut ini:
+        //isEmty()
+
+        System.out.println(nama[0].isEmpty());     
+        System.out.println(nama[1].isEmpty());     
+        System.out.println(nama[2].isEmpty());     
+        System.out.println(nama[3].isEmpty());     
+    } 
+}
